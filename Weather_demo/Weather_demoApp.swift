a45b23272b11507a3cb297e12610fdf15812f578
Jenyo Oyebode <jenyooyebode@icloud.com>
2023-09-26 14:58:57 -0400

    Initial Commit

---
+++ Weather_demo/Weather_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_demoApp.swift
+//  Weather_demo
+//
+//  Created by Jenyo Oyebode on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
